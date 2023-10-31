bec910424c0ee84d7ee660a3ccd5b95fdd14a0ec
Daniel Arcé <daniel.arce@gmail.com>
2023-10-31 17:27:52 -0400

    remove footer comment

--- footer.php
+++ footer.php
@@ -14,21 +14,7 @@
 	<footer id="colophon" class="site-footer">
 		<span class = "site-info">
 		&copy; <?php echo date('Y'); ?> Debra McCall
-		</span>
-		<!-- <div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bauhaus-dances' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'bauhaus-dances' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bauhaus-dances' ), 'bauhaus-dances', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div> -->
-		<!-- .site-info -->
+		</span><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
